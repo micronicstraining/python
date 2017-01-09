f7ec9049eed30cf5d6eea815f6931ac949d38ddb
tetriseffect <tigran.avakyan@icloud.com>
2017-01-09 02:56:28 +0000

    Added some shell guidance instructions and a way to access OS environment variables from pythonw

--- module_1/navigating_bash/nav.sh
+++ module_1/navigating_bash/nav.sh
@@ -1,16 +1,33 @@
+# based on user id give me current user name
 man whoami
 whoami
 sudo whoami
 
-man env
-env
-
+# find directory where binary is located
 man which
 which python
 which python3
 which pip
 which pip3
 
+# global PATH definition
+cat /etc/environment
+
+# output all environmental variables
+env
+
+# adding stuff to PATH variable (typically in .bashrc or the like)
 ls 
 ls -A
 grep -R 'usr/local/bin'
+
+
+# output a process's environment variables
+# sudo cat /proc/866/environ 
+# sudo cat /proc/866/environ | tr '\0' '\n'
+
+
+# lab install zsh
+
+# lab install zonsh
+
---
+++ module_1/navigating_bash/os_env.py
@@ -0,0 +1,9 @@
+#! /bin/python3
+
+import os
+
+
+print('ENV variable is: {0}'.format(os.environ))
+
+
+print('PATH variable is: {0}'.format(os.environ['PATH']))
