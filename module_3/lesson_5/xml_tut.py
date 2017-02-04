73a3b9c26e2526ac3c09a95a296aa3fd86bc419d
tetriseffect <tigran.avakyan@icloud.com>
2017-02-04 19:31:47 +0000

    updated lessons for XMLw

--- module_3/lesson_5/xml_tut.py
+++ module_3/lesson_5/xml_tut.py
@@ -1,6 +1,31 @@
-# xml lab + problems
-# hackerrank problems
-# xml
-# - XML overview
+#! /usr/bin/env python3
+
+# Lab: Solve Hacker Rank XML problems
+
+# See https://docs.python.org/3.6/library/xml.etree.elementtree.html for more details
+
+import xml.etree.ElementTree as ET
+
+# This is like the JSON.loads
+tree = ET.parse('country.xml')
+root = tree.getroot()
+
+print(root.tag)
+print(root.attrib)
+
+for child in root:
+    print(child.tag, child.attrib)
+
+
+print(root[0][1].text)
+
+# buidling XML
+data = ET.Element('data')
+country = ET.SubElement(data, 'country')
+rank = ET.SubElement(country, 'rank')
+year = ET.SubElement(country, 'year')
+print(ET.dump(data))
+
+# Will go over following details if asked
+# - XML in depth overview
 # - XSLT overview, XPATH overview
-# - XML exercises hacker rank
